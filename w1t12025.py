237090388e42a44ba2103da253dbd4df34a48c1c
Quill <thebrosnami@gmail.com>
2025-02-04 00:25:58 +1300

    day1

---
+++ w1t12025.py
@@ -0,0 +1,70 @@
+""""
+#Si 1 and Si 5 simple calculator 
+print('Enter a number')
+x = int(input())
+print('Enter another number')
+y = int(input())
+print('Enter a number dpending on what you want')
+print('1 = add, 2 = minus, 3 = times, 4 = Divide')
+z = int(input())
+
+
+if z == 1:
+    a = x + y
+    print (a)
+
+4
+elif z == 2:
+    b = x - y
+    print (b)
+
+elif z == 3:
+    c = x * y
+    print (c) 
+
+elif z == 4:
+    d = x / y
+    print (d)
+
+else:
+    print("try again")
+
+"""
+#even or odd
+print("enter a number")
+num = int(input())
+
+if num % 2 == 0:
+    print("that number is even")
+else:
+    print("that number is odd")
+
+
+#SI 3 getting the right age
+print ("enter age")
+age = int(input())
+if age >= 18:
+    print("you are old enough to vote")
+
+else:
+    print("you are NOT old enough")
+
+#Si 4 calculating 
+
+a = 5 * 2 + 3 
+print(a)
+'''
+5 * 2 + 3 the code is lined up to solve the soultion
+* is multiplying the 5 and the 2 getting a result of 10
+after its adding together the 10 and the 3 getting the result of 13
+the "a" at the start is taking the result of the result which is 13 
+and the print(a) is printing the result which gives us the 13 
+'''
+#Si 5 
+
+
+'''
+x/2 = y
+if y  
+print (x)
+'''
